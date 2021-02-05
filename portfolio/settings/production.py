082e9dd8023493b5631ac71b7b1e0c94fbbfe088
Cameron Hyde <pluckthechicken@hotmail.co.uk>
2021-02-05 12:52:29 +1000

    Configure logging. Catch error if ticker doesn't exist

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -1,5 +1,6 @@
 """Portfolio production settings."""
 
+import os
 from .base import *
 
 DEBUG = False
@@ -19,6 +20,48 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
     }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} | {asctime} | {module}: {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'file': {
+            'delay': True,
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'maxBytes': 10000000,  # 10MB ~ 200k rows
+            'backupCount': 5,
+            'filename': os.path.join(BASE_DIR, 'portfolio/logs/main.log'),
+            'formatter': 'verbose',
+        },
+        'error_file': {
+            'delay': True,
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'maxBytes': 10000000,  # 10MB ~ 200k rows
+            'backupCount': 5,
+            'filename': os.path.join(BASE_DIR, 'portfolio/logs/error.log'),
+            'formatter': 'verbose',
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file', 'error_file', 'console'],
+            'level': 'INFO',
+        },
+    },
 }
 
 STATIC_ROOT = os.path.join(
